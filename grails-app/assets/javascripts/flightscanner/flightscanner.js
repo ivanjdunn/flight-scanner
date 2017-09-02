c78a0e3052f2d7e375b2f56729a891ed3e789794
ivan dunn <ivan.dunn@hmhco.com>
2017-09-02 16:44:26 +0100

    initial commit

---
+++ grails-app/assets/javascripts/flightscanner/flightscanner.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /flightscanner/core/flightscanner.core
+//= require /flightscanner/index/flightscanner.index
+
+angular.module("flightscanner", [
+        "flightscanner.core",
+        "flightscanner.index"
+    ]);
---
+++ grails-app/init/flightscanner/BootStrap.groovy
@@ -0,0 +1,9 @@
+package flightscanner
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
