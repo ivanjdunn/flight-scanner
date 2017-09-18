e9862ca8dba72539f50840ab55c863411cc9a69e
ivan dunn <ivan.dunn@hmhco.com>
2017-09-18 20:31:58 +0100

    overriding toString for Flight and Route Classes

--- src/main/groovy/com/ryanair/Flight.groovy
+++ src/main/groovy/com/ryanair/Flight.groovy
@@ -1,8 +1,11 @@
 package com.ryanair
 
 import groovy.transform.CompileStatic
+import groovy.transform.ToString
+
 import java.time.LocalTime
 
+@ToString( includeNames=true )
 @CompileStatic
 class Flight {	
 	
--- src/main/groovy/com/ryanair/Route.groovy
+++ src/main/groovy/com/ryanair/Route.groovy
@@ -1,7 +1,9 @@
 package com.ryanair
 
 import groovy.transform.CompileStatic
+import groovy.transform.ToString
 
+@ToString(includeNames=true, excludes='newRoute, seasonalRoute, group')
 @CompileStatic
 class Route {
 
