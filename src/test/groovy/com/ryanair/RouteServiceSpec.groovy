68b916563492a5867576eae1dad46fea15946d2d
ivan dunn <ivan.dunn@hmhco.com>
2017-11-20 13:16:13 +0000

    Added Unit tests

--- src/test/groovy/com/ryanair/RouteServiceSpec.groovy
+++ src/test/groovy/com/ryanair/RouteServiceSpec.groovy
@@ -5,43 +5,41 @@ import grails.testing.services.ServiceUnitTest
 import spock.lang.Specification
 
 
-class RouteServiceSpec extends Specification implements ServiceUnitTest<RouteService> {	
+class RouteServiceSpec extends Specification implements ServiceUnitTest<RouteService> {
 
-/*	
-def "Routes endpoint is called once and returns 200 OK response"() {	
-	
-	given: 
-		ErsatzServer ersatz = new ErsatzServer()
-		String departureAirport = "DUB"
-		String arrivalAirport = "STN"
-		
-		ersatz.expectations {			
-			get('/'){				
-				called(1)				
-				responder {					
-					code(200)
-					content expected,'application/json'
-				}
-			}			
-		}
-	
-		ersatz.start()
-		service.ryanairUrl = ersatz.httpUrl
-		
-		when: 
-		def availableRoute = service.availableRoute(departureAirport, arrivalAirport)
-		
-		then: 
-		println availableRoute
-		ersatz.verify()
-		
-		
-		cleanup:
-		ersatz.stop()
-		
-	}
-*/
-	
-	
+
+    def "Routes endpoint is called once and returns 200 OK response"() {
+
+        given:
+        ErsatzServer ersatz = new ErsatzServer()
+        String departureAirport = "DUB"
+        String arrivalAirport = "STN"
+
+        ersatz.expectations {
+            get('/'){
+                called(1)
+                responder {
+                    code(200)
+                    content expected,'application/json'
+                }
+            }
+        }
+
+        ersatz.start()
+        service.routesApi = ersatz.httpUrl
+
+        when:
+        def availableRoute = service.availableRoute(new Airport(departureAirport), new Airport(arrivalAirport))
+
+        then:
+        println availableRoute
+        ersatz.verify()
+
+
+        cleanup:
+        ersatz.stop()
+
+    }
+
 }
 
--- src/test/groovy/com/ryanair/ScheduleServiceSpec.groovy
+++ src/test/groovy/com/ryanair/ScheduleServiceSpec.groovy
@@ -3,4 +3,31 @@ package com.ryanair
 import grails.testing.services.ServiceUnitTest
 import spock.lang.Specification
 
-class ScheduleServiceSpec extends Specification implements ServiceUnitTest<ScheduleService>{}
+class ScheduleServiceSpec extends Specification implements ServiceUnitTest<ScheduleService>{
+
+    @spock.lang.Shared
+    def baseUrl = "https://api.ryanair.com/timetable/3/schedules/"
+
+
+    @spock.lang.Unroll("A base Url, a route Object, flight year and flight month then return an expected URL")
+    def "validate url Builder"(){
+
+        given: "A shared baseUrl"
+
+        when: "Schedule-Service url Builder method is called"
+        def returnedUrl = service.urlBuilder(routeObject, flightYear, flightMonth)
+
+        then: "The generated url == expectedUrl"
+        returnedUrl == expectedUrl
+
+        where:
+        routeObject	 					                      | flightYear   | flightMonth     || expectedUrl
+        new Route(airportFrom:"DUB", airportTo: "WRO")		  | 2017         | 10		       || "${getBaseUrl()}DUB/WRO/years/2017/months/10"
+        new Route(airportFrom:"WRO", airportTo: "DUB")		  | 2017         | 10		       || "${getBaseUrl()}WRO/DUB/years/2017/months/10"
+        new Route(airportFrom:"DUB", airportTo: "STN")		  | 2018         | 12		       || "${getBaseUrl()}DUB/STN/years/2018/months/12"
+        new Route(airportFrom:"DUB", airportTo: "STN")		  | null         | null		       || "${getBaseUrl()}DUB/STN/years/null/months/null"
+        new Route(airportFrom:"DUB", airportTo: "STN")		  | 2020         | 01		       || "${getBaseUrl()}DUB/STN/years/2020/months/1"
+
+    }
+
+}
