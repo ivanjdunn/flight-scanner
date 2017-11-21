92ee458454ba351201cac10fba45dd35eab78d5c
ivan dunn <ivan.dunn@hmhco.com>
2017-11-21 14:15:44 +0000

    outputting JSON response in the required format

--- grails-app/services/com/ryanair/FlightSelectorService.groovy
+++ grails-app/services/com/ryanair/FlightSelectorService.groovy
@@ -10,75 +10,86 @@ class FlightSelectorService {
 
 	def selectedFlights(List<AvailableFlight> availableSchedule, LocalDateTime earliestDeparture, LocalDateTime latestArrival, Airport departureAirport, Airport arrivalAirport) {
 
-		// get direct flights
+		// get direct flight
 		List directFlights = availableSchedule.findAll{ it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode()  }
+		//jsonFlightBuilder( directFlights, earliestDeparture, latestArrival )
+
+		//get indirect flights
+		List indirectFlights = []
+		List allIndirect = availableSchedule - directFlights
+		List leg1Listing = allIndirect.findAll { it.departureAirport == departureAirport.getIataCode() }
+		List leg2Listing = allIndirect - leg1Listing
+
+		leg1Listing.each { leg1 ->
+
+			leg2Listing.each { leg2 ->
+
+				if(leg1.arrivalAirport == leg2.departureAirport) {
+
+					List bothLegs = []
+
+					List leg1List = []
+					leg1List << leg1
+					List leg2List = []
+					leg2List << leg2
+
+					bothLegs << leg1List << leg2List
+
+					indirectFlights << bothLegs
+
+				}
+			}
+
+		} // end outer loop
+
+		List targetFlights = [] << directFlights << indirectFlights
+
 		jsonFlightBuilder( directFlights, earliestDeparture, latestArrival )
 
 	}
 
 
 
-	def jsonFlightBuilder( List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival ){
-
-		new StringWriter().with { w ->
-			def json = new groovy.json.StreamingJsonBuilder(w)
-
-			json flights, { def flight ->
-
-				stops 0
-
-				flight.flightList.each{
-
-					def time = it
-					// customer provided time boundaries
-					if (time?.departureTime.isAfter(earliestDeparture.toLocalTime()) && time?.arrivalTime.isBefore(latestArrival.toLocalTime())){
-
-						legs{
-							departureAirport flight?.departureAirport
-							arrivalAirport flight?.arrivalAirport
-							departureDateTime LocalDate.now().atTime(time?.departureTime).toString()
-							arrivalDateTime LocalDate.now().atTime(time?.arrivalTime).toString()
-						}
-					}
-
-				}
-			}
-
-			return w.toString()
-
-		}
-
-	}
-
-
-	def alternativeJsonResonse ( List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival ) {
+	def jsonFlightBuilder(List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival ) {
 
 		new StringWriter().with { w ->
 			def jsonBuilder = new groovy.json.StreamingJsonBuilder(w)
 
-			jsonBuilder flights, { def flight ->
+			flights.each { flight ->
+
+				def criteriaMatched = []
+
+				flight.flightList.each { flightListInstance ->
+
+					if (flightListInstance?.departureTime.isAfter(earliestDeparture.toLocalTime()) && flightListInstance?.arrivalTime.isBefore(latestArrival.toLocalTime())) {
+
+						criteriaMatched << flightListInstance
+
+					}
+				}
 
 				jsonBuilder(
-						flight.flightList.collect {
-							def time = it
-							[
-								stops: 0,
-								legs : [
-									departureAirport: flight?.departureAirport,
-									arrivalAirport: flight?.arrivalAirport,
-									departureDateTime: LocalDate.now().atTime(time?.departureTime).toString(),
-									arrivalDateTime: LocalDate.now().atTime(time?.arrivalTime).toString()
+						criteriaMatched.collect { time ->
 
-								]
+							[
+									stops: 0,
+									legs : [
+											departureAirport : flight?.departureAirport,
+											arrivalAirport   : flight?.arrivalAirport,
+											departureDateTime: LocalDate.now().atTime(time?.departureTime).toString(),
+											arrivalDateTime  : LocalDate.now().atTime(time?.arrivalTime).toString()
+
+									]
 							]
 						}
-						)
+				)
+
 			}
-
-
 			return w.toString()
 		}
 
 	}
+
+
 
 }
