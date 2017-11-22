76d97339de11d0fd199d659f1ccd9192a5d4a3dd
ivan dunn <ivan.dunn@hmhco.com>
2017-11-22 18:34:35 +0000

    refactored JSON Builder method removing time logic into its own method

--- grails-app/services/com/ryanair/FlightSelectorService.groovy
+++ grails-app/services/com/ryanair/FlightSelectorService.groovy
@@ -9,15 +9,13 @@ class FlightSelectorService {
 
     def selectFlights(List<AvailableFlight> availableSchedule, LocalDateTime earliestDeparture, LocalDateTime latestArrival, Airport departureAirport, Airport arrivalAirport) {
 
-        List directFlights = availableSchedule.findAll {
-            it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode()
-        }
+        //List directFlights = availableSchedule.findAll { it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode() }
+        //def directFlight = availableSchedule.find { it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode() }
 
         List indirectFlights = getIndirectFlights(availableSchedule, departureAirport, arrivalAirport)
+        def buildFlightCriteria = getTargetFlights(indirectFlights, earliestDeparture, latestArrival)
 
-        // jsonFlightBuilder(directFlights, earliestDeparture, latestArrival)
-
-        jsonFlightBuilder2(indirectFlights, earliestDeparture, latestArrival)
+        jsonFlightBuilder(buildFlightCriteria)
 
     }
 
@@ -27,6 +25,14 @@ class FlightSelectorService {
 
         List leg1Listing = availableSchedule.findAll { it.departureAirport == departureAirport.getIataCode() }
         List leg2Listing = availableSchedule - leg1Listing
+
+        /*// find direct-Flight
+        def directFlight = availableSchedule.find{ it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode() }
+        if (directFlight){
+
+            indirectFlights << new IndirectFlight(leg1: directFlight)
+
+        }*/
 
         // e.g. DUB - WRO
         leg1Listing.each { leg1 ->
@@ -46,100 +52,57 @@ class FlightSelectorService {
     }
 
 
-    def jsonFlightBuilder2(List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival) {
+    def getTargetFlights(List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival){
+
+        def criteriaMatched = []
+
+        flights.each { flight ->
+
+            flight.leg1.flightList.each { flightListInstance ->
+
+                if (flightListInstance?.departureTime.isAfter(earliestDeparture.toLocalTime()) && flightListInstance?.arrivalTime.isBefore(latestArrival.toLocalTime())) {
+
+                    // find suitable leg2 flight - leaves 2 hours after leg1 flight lands and leg2 arrives within the time boundaries
+                    def findSuitableLeg2Flight = flight.leg2?.flightList.find { it.departureTime.isAfter(flightListInstance?.departureTime.plusHours(2L)) && it.arrivalTime.isBefore(latestArrival.toLocalTime())}
+
+                    if (findSuitableLeg2Flight) {
+
+                        def required = [leg1DepartureAirport: flight.leg1?.departureAirport, leg1ArivalAirport: flight.leg1?.arrivalAirport, leg1DepartureTime: flightListInstance?.departureTime,
+                                        leg1ArrivalTime: flightListInstance?.arrivalTime, leg2DepartureAirport: flight.leg2?.departureAirport, leg2ArivalAirport: flight.leg2?.arrivalAirport,
+                                        leg2DepartureTime: findSuitableLeg2Flight?.departureTime, leg2ArrivalTime: findSuitableLeg2Flight?.arrivalTime]
+
+
+                        criteriaMatched << required
+                    }
+                }
+            }
+        } // end outer each
+
+        return criteriaMatched
+
+
+    }
+
+
+    def jsonFlightBuilder(def flightCriteria) {
 
         new StringWriter().with { w ->
             def jsonBuilder = new groovy.json.StreamingJsonBuilder(w)
 
-            def criteriaMatched = []
-
-            flights.each { flight ->
-
-                flight.leg1.flightList.each { flightListInstance ->
-
-                    if (flightListInstance?.departureTime.isAfter(earliestDeparture.toLocalTime()) && flightListInstance?.arrivalTime.isBefore(latestArrival.toLocalTime())) {
-
-                        // find suitable leg2 flight - leaves 2 hours after leg1 flight lands and leg2 arrives within the time boundaries
-                        def findSuitableLeg2Flight = flight.leg2.flightList.find {
-                            it.departureTime.isAfter(flightListInstance?.departureTime.plusHours(2L)) && it.arrivalTime.isBefore(latestArrival.toLocalTime())
-                        }
-
-                        if (findSuitableLeg2Flight) {
-
-                            def required = [leg1DepartureAirport: flight.leg1.departureAirport, leg1ArivalAirport: flight.leg1.arrivalAirport, leg1DepartureTime: flightListInstance?.departureTime,
-                                            leg1ArrivalTime     : flightListInstance?.arrivalTime, leg2DepartureAirport: flight.leg2.departureAirport, leg2ArivalAirport: flight.leg2.arrivalAirport,
-                                            leg2DepartureTime   : findSuitableLeg2Flight?.departureTime, leg2ArrivalTime: findSuitableLeg2Flight?.arrivalTime]
-
-
-                            criteriaMatched << required
-                        }
-
-                    }
-                }
-            } // end outer each
-
             jsonBuilder(
-                    criteriaMatched.collect { time ->
+                    flightCriteria.collect { time ->
 
                         [
                                 stops: 1,
                                 legs : [
-
                                         [departureAirport: time.leg1DepartureAirport, arrivalAirport: time.leg1ArivalAirport, departureDateTime: LocalDate.now().atTime(time?.leg1DepartureTime).toString(), arrivalDateTime: LocalDate.now().atTime(time?.leg1ArrivalTime).toString()],
-
-                                        //departureDateTime: LocalDate.now().atTime(time?.departureTime).toString(),
-                                        //arrivalDateTime: LocalDate.now().atTime(time?.arrivalTime).toString()
                                         [departureAirport: time.leg2DepartureAirport, arrivalAirport: time.leg2ArivalAirport, departureDateTime: LocalDate.now().atTime(time?.leg2DepartureTime).toString(), arrivalDateTime: LocalDate.now().atTime(time?.leg2ArrivalTime).toString()]
                                 ]
-
-
                         ]
 
                     }
             )
 
-
-            return w
-        }
-
-    }
-
-
-    def jsonFlightBuilder(List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival) {
-
-        new StringWriter().with { w ->
-            def jsonBuilder = new groovy.json.StreamingJsonBuilder(w)
-
-            flights.each { flight ->
-
-                def criteriaMatched = []
-
-                flight.flightList.each { flightListInstance ->
-
-                    if (flightListInstance?.departureTime.isAfter(earliestDeparture.toLocalTime()) && flightListInstance?.arrivalTime.isBefore(latestArrival.toLocalTime())) {
-
-                        criteriaMatched << flightListInstance
-
-                    }
-                }
-
-                jsonBuilder(
-                        criteriaMatched.collect { time ->
-
-                            [
-                                    stops: 0,
-                                    legs : [
-                                            departureAirport: flight?.departureAirport,
-                                            arrivalAirport: flight?.arrivalAirport,
-                                            departureDateTime: LocalDate.now().atTime(time?.departureTime).toString(),
-                                            arrivalDateTime: LocalDate.now().atTime(time?.arrivalTime).toString()
-
-                                    ]
-                            ]
-                        }
-                )
-
-            }
             return w
         }
 
