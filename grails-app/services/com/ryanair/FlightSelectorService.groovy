a871fb5841146e2c70b3a03200ad77ade9a53fa8
ivan dunn <ivan.dunn@hmhco.com>
2017-11-22 16:59:05 +0000

    JSON ouput handles interconnected flights

--- grails-app/services/com/ryanair/FlightSelectorService.groovy
+++ grails-app/services/com/ryanair/FlightSelectorService.groovy
@@ -9,11 +9,15 @@ class FlightSelectorService {
 
     def selectFlights(List<AvailableFlight> availableSchedule, LocalDateTime earliestDeparture, LocalDateTime latestArrival, Airport departureAirport, Airport arrivalAirport) {
 
-        List directFlights = availableSchedule.findAll { it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode() }
+        List directFlights = availableSchedule.findAll {
+            it.departureAirport == departureAirport.getIataCode() && it.arrivalAirport == arrivalAirport.getIataCode()
+        }
 
         List indirectFlights = getIndirectFlights(availableSchedule, departureAirport, arrivalAirport)
 
-        jsonFlightBuilder(directFlights, earliestDeparture, latestArrival)
+        // jsonFlightBuilder(directFlights, earliestDeparture, latestArrival)
+
+        jsonFlightBuilder2(indirectFlights, earliestDeparture, latestArrival)
 
     }
 
@@ -39,6 +43,65 @@ class FlightSelectorService {
         }
 
         return indirectFlights
+    }
+
+
+    def jsonFlightBuilder2(List flights, LocalDateTime earliestDeparture, LocalDateTime latestArrival) {
+
+        new StringWriter().with { w ->
+            def jsonBuilder = new groovy.json.StreamingJsonBuilder(w)
+
+            def criteriaMatched = []
+
+            flights.each { flight ->
+
+                flight.leg1.flightList.each { flightListInstance ->
+
+                    if (flightListInstance?.departureTime.isAfter(earliestDeparture.toLocalTime()) && flightListInstance?.arrivalTime.isBefore(latestArrival.toLocalTime())) {
+
+                        // find suitable leg2 flight - leaves 2 hours after leg1 flight lands and leg2 arrives within the time boundaries
+                        def findSuitableLeg2Flight = flight.leg2.flightList.find {
+                            it.departureTime.isAfter(flightListInstance?.departureTime.plusHours(2L)) && it.arrivalTime.isBefore(latestArrival.toLocalTime())
+                        }
+
+                        if (findSuitableLeg2Flight) {
+
+                            def required = [leg1DepartureAirport: flight.leg1.departureAirport, leg1ArivalAirport: flight.leg1.arrivalAirport, leg1DepartureTime: flightListInstance?.departureTime,
+                                            leg1ArrivalTime     : flightListInstance?.arrivalTime, leg2DepartureAirport: flight.leg2.departureAirport, leg2ArivalAirport: flight.leg2.arrivalAirport,
+                                            leg2DepartureTime   : findSuitableLeg2Flight?.departureTime, leg2ArrivalTime: findSuitableLeg2Flight?.arrivalTime]
+
+
+                            criteriaMatched << required
+                        }
+
+                    }
+                }
+            } // end outer each
+
+            jsonBuilder(
+                    criteriaMatched.collect { time ->
+
+                        [
+                                stops: 1,
+                                legs : [
+
+                                        [departureAirport: time.leg1DepartureAirport, arrivalAirport: time.leg1ArivalAirport, departureDateTime: LocalDate.now().atTime(time?.leg1DepartureTime).toString(), arrivalDateTime: LocalDate.now().atTime(time?.leg1ArrivalTime).toString()],
+
+                                        //departureDateTime: LocalDate.now().atTime(time?.departureTime).toString(),
+                                        //arrivalDateTime: LocalDate.now().atTime(time?.arrivalTime).toString()
+                                        [departureAirport: time.leg2DepartureAirport, arrivalAirport: time.leg2ArivalAirport, departureDateTime: LocalDate.now().atTime(time?.leg2DepartureTime).toString(), arrivalDateTime: LocalDate.now().atTime(time?.leg2ArrivalTime).toString()]
+                                ]
+
+
+                        ]
+
+                    }
+            )
+
+
+            return w
+        }
+
     }
 
 
